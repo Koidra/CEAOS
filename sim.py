3e1439d5c95cd13d4470413c562fbf065182e02f
Kenneth Tran <ken@koidra.ai>
2020-08-08 02:08:52 -0400

    refactor name

--- sim.py
+++ sim.py
@@ -1,26 +1,20 @@
 import numpy as np
-
+import np.ndarray as vec
 from climate_model import IndoorClimateModel
 from crop_model import CropModel
 
 
-class CEASim():
-
+class Simulator(object):
     def __init__(self, climate_model: IndoorClimateModel, crop_model: CropModel):
         self.climate_model = climate_model
         self.crop_model = crop_model
         self._crop_obs = NotImplemented
 
-    def step_(self, climate_setpoint: np.ndarray, crop_setpoint: np.ndarray) -> np.ndarray:
+    def step(self, climate_setpoint: vec, crop_setpoint: vec) -> vec:
         climate_obs = self.climate_model.step(climate_setpoint, self._crop_obs)
         crop_obs = self.crop_model.step(climate_obs, crop_setpoint)
 
         self._crop_obs = crop_obs
-        raise NotImplementedError
-
-    # Implement gym's interface
-    def step(self, setpoint: np.ndarray):
-        # TODO: Call self._step
         raise NotImplementedError
 
     def reset(self):
