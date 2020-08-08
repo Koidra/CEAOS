a218542ce7cb2657d573ae2480ef30b423bdc2a7
Kenneth Tran <ken@koidra.ai>
2020-08-08 02:34:35 -0400

    add ode solver

---
+++ ode_solver.py
@@ -0,0 +1,22 @@
+from collections import Callable
+from typing import List
+from numpy import ndarray as vec
+from numpy import ndarray as mat
+
+
+def rk4(A: mat, x: vec, h: float) -> vec:
+    """
+    This is an implementation of Runge-Kutta 4th order method
+    A: square matrix encapsulating the dynamics. A is assumed to be constant during the step.
+    x: state variable at time t
+    return x at t+h
+    """
+    k1 = A * x
+    k2 = A * (x + h * k1 / 2)
+    k3 = A * (x + h * k2 / 2)
+    k4 = A * (x + h * k3)
+    return x + h/6 * (k1 + 2*k2 + 2*k3 + k4)
+
+
+def multistep_solve(f: Callable, xs: List[vec], h: float) -> vec:
+    raise NotImplementedError()
