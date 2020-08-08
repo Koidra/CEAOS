6938b294836442805f2da928145d9211fc931336
Kenneth Tran <ken@koidra.ai>
2020-08-08 03:16:35 -0400

    add adams_moulton

--- ode_solver.py
+++ ode_solver.py
@@ -1,8 +1,7 @@
 from collections import Callable
 from typing import List
-from numpy import ndarray as vec
-from numpy import ndarray as mat
-
+import numpy as np
+from numpy import ndarray as vec, mat
 
 def rk4(A: mat, x: vec, h: float) -> vec:
     """
@@ -18,5 +17,14 @@ def rk4(A: mat, x: vec, h: float) -> vec:
     return x + h/6 * (k1 + 2*k2 + 2*k3 + k4)
 
 
-def multistep_solve(f: Callable, xs: List[vec], h: float) -> vec:
-    raise NotImplementedError()
+def adams_moulton(As: List[mat], xs: List[vec], h: float) -> vec:
+    """
+    Adams-Moulton is a linear multistep method which uses the information from the previous steps
+    https://en.wikipedia.org/wiki/Linear_multistep_method#Adams%E2%80%93Moulton_methods
+    """
+    a = np.eye(len(xs[0])) - h * 251/720 * As[4]
+    b = xs[3] + h * (646 * As[3] * xs[3]
+                 - 264 * As[2] * xs[2]
+                 + 106 * As[1] * xs[1]
+                 - 19 * As[0] * xs[0]) / 720
+    return np.linalg.solve(a, b)
